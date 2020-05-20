e1bd780ab59e03e35c5fa75f2c513e9e8bacdc58
Brad R. Allen <bradrallen@gmail.com>
2020-05-20 00:18:48 -0600

    nave

--- web/common/scripts.php
+++ web/common/scripts.php
@@ -1,7 +1,7 @@
 <!-- scripts -->
-<script src="../js/vendor/modernizr-3.8.0.min.js"></script>
+<script src="/app/web/js/vendor/modernizr-3.8.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="../js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
-  <script src="../js/plugins.js"></script>
-  <script src="../js/main.js"></script>
-  <script src="../js/bootstrap.bundle.min.js"></script>
+  <script>window.jQuery || document.write('<script src="/app/web/js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+  <script src="/app/web/js/plugins.js"></script>
+  <script src="/app/web/js/main.js"></script>
+  <script src="/app/web/js/bootstrap.bundle.min.js"></script>
