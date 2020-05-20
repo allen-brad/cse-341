b90a47c34006125586ea963ee2920d08d8a6d4e3
Brad R. Allen <bradrallen@gmail.com>
2020-05-20 00:08:44 -0600

    relative paths

--- web/w03/cart.php
+++ web/w03/cart.php
@@ -63,12 +63,12 @@ switch ($action) {
   <title>CS341 W03 Assignment | Cart</title>
 
   <link rel="manifest" href="../site.webmanifest">
-  <link rel="apple-touch-icon" href="../cropped-ba-192x192.png">
+  <link rel="apple-touch-icon" href="./cropped-ba-192x192.png">
 
   <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../css/main.css">
-  <link rel="stylesheet" href="../css/w03.css">
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./css/main.css">
+  <link rel="stylesheet" href="./css/w03.css">
 
 </head>
 
@@ -77,7 +77,7 @@ switch ($action) {
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
   <![endif]-->
 
-  <?php include $_SERVER['DOCUMENT_ROOT'].'../common/nav.php'; ?>
+  <?php include $_SERVER['DOCUMENT_ROOT'].'./common/nav.php'; ?>
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="display-4">Cart</h1>
         <?php
@@ -143,7 +143,7 @@ switch ($action) {
         </div>
     </div>
 
-    <?php include $_SERVER['DOCUMENT_ROOT'].'../common/sripts.php';?>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'./common/sripts.php';?>
 
 </body>
 
