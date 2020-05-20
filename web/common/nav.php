7674f9a10c1e1d622768b19e9ec764bee53f11ff
Brad R. Allen <bradrallen@gmail.com>
2020-05-19 23:44:38 -0600

    nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-light fixed-top" style="background-color: #e3f2fd;">
-  <a class="navbar-brand" href="index.php"><img src="img/cropped-ba-32x32.png" alt="BA Home"></a>
+  <a class="navbar-brand" href="index.php"><img src="/img/cropped-ba-32x32.png" alt="BA Home"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -13,7 +13,7 @@
             <div class="dropdown-menu" aria-labelledby="dropdown01">
               <a class="dropdown-item <?php if($_SERVER['SCRIPT_NAME']=="/w01/index.php") echo 'active'; ?>" href="/w01/">Week 01</a>
               <a class="dropdown-item <?php if($_SERVER['SCRIPT_NAME']=="/w02/index.php") echo 'active'; ?>" href="/w02/">Week 02</a>
-              <a class="dropdown-item <?php if($_SERVER['SCRIPT_NAME']=="/w03/index.php") echo 'active'; ?>" href="/w03/">Week 03</a>
+              <a class="dropdown-item <?php if(getcwd()=="/app/web/w03") echo 'active'; ?>" href="/w03/">Week 03</a>
               <a class="dropdown-item <?php if($_SERVER['SCRIPT_NAME']=="/w04/index.php") echo 'active'; ?>" href="/w4/">Week 04</a>
               <a class="dropdown-item <?php if($_SERVER['SCRIPT_NAME']=="/w05/index.php") echo 'active'; ?>" href="/w5/">Week 05</a>
 
@@ -21,7 +21,7 @@
       </li>
     </ul>
     <?php
-      if(getcwd()=="/web/w03"){
+      if(getcwd()=="/app/web/w03"){
         echo '<div class="btn-group" role="group" aria-label="Cart Buttons">';
         //echo '<form class="form-inline mt-2 mt-md-0">';
         echo '<button type="button" onclick="document.location = \'/w03/cart.php\'" class="btn btn-secondary">Cart: ' . itemCountInCart() . '</button>';
