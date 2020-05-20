35b39ac41ee4427b7063cdf4e5ae9a032639a5c1
Brad R. Allen <bradrallen@gmail.com>
2020-05-20 00:17:52 -0600

    paths

--- web/w03/cart.php
+++ web/w03/cart.php
@@ -15,9 +15,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-include $_SERVER['DOCUMENT_ROOT'].'/library/friut_functions.php';
+include $_SERVER['DOCUMENT_ROOT'].'/app/web/library/friut_functions.php';
 
-include $_SERVER['DOCUMENT_ROOT'].'/library/friuts.php';
+include $_SERVER['DOCUMENT_ROOT'].'/app/web/library/friuts.php';
 
 
 /*_____________________ actions _____________________*/
@@ -62,13 +62,13 @@ switch ($action) {
 
   <title>CS341 W03 Assignment | Cart</title>
 
-  <link rel="manifest" href="../site.webmanifest">
-  <link rel="apple-touch-icon" href="./cropped-ba-192x192.png">
+  <link rel="manifest" href="/app/web/site.webmanifest">
+  <link rel="apple-touch-icon" href="/app/web/cropped-ba-192x192.png">
 
   <!-- Bootstrap core CSS -->
-  <link href="./css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="./css/main.css">
-  <link rel="stylesheet" href="./css/w03.css">
+  <link href="/app/web/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/app/web/css/main.css">
+  <link rel="stylesheet" href="/app/web/css/w03.css">
 
 </head>
 
@@ -77,7 +77,7 @@ switch ($action) {
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
   <![endif]-->
 
-  <?php include $_SERVER['DOCUMENT_ROOT'].'./common/nav.php'; ?>
+  <?php include $_SERVER['DOCUMENT_ROOT'].'app/web/common/nav.php'; ?>
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="display-4">Cart</h1>
         <?php
@@ -143,7 +143,7 @@ switch ($action) {
         </div>
     </div>
 
-    <?php include $_SERVER['DOCUMENT_ROOT'].'./common/sripts.php';?>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/app/web/common/sripts.php';?>
 
 </body>
 
